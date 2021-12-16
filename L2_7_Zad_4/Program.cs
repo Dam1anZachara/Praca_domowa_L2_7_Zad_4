6bf7ba6270462d19c1b5757577a9b85881e6b0fb
dzachara <dzachara@ZACHARA-D>
2021-12-16 13:33:50 +0100

    Add project files.

---
+++ L2_7_Zad_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace L2_7_Zad_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ten program sprawdzi czy wpisany rok jest przestępny.");
+            Console.Write("Podaj rok: ");
+
+            Int32.TryParse(Console.ReadLine(), out int yearToCheck);
+
+            if ((yearToCheck % 4 == 0) && (yearToCheck % 100 != 0) || (yearToCheck % 400 == 0))
+            {
+                Console.WriteLine($"Wpisany rok {yearToCheck} jest przestępny.");
+            } 
+            else
+            {
+                Console.WriteLine($"Wpisany rok {yearToCheck} nie jest przestępny.");
+            }
+        }
+    }
+}
